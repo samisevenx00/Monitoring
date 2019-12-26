a973cd376c11826c316736e84a22b1a69b69adfb
samisevenx00 <shocini@gmail.com>
2019-12-26 11:07:16 +0100

    push du script qui servira a récupérer des metrics depuis UptimeRobot

---
+++ API_UptimeRobot.ps1
@@ -0,0 +1,7 @@
+﻿$Api_key = $env:APIKEY_UPTROBOT
+$uri = "https://api.uptimerobot.com/v2/"
+$params = @{'api_key' = $Api_key; 'format'= 'json'; 'logs' =1}
+
+$Method = "getMonitors"
+
+$Url_Monitored = Invoke-RestMethod -Uri ($uri + $Method) -Method Post -Body $params
